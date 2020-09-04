4cd528f33304a859daf5659ab1686f3b4aee5f37
QuiteGuyIsaiah <591952@bah.com>
2020-09-04 19:31:07 -0400

    Sign example page for vendor

---
+++ js/functions.js
@@ -0,0 +1,18 @@
+function setSecction(value) {
+ 
+ /*
+    
+                document.getElementById(value).style='display:block;';
+    
+    var i;
+     for(i=0; i < 5; i++){
+            document.getElementById('2').style='display:none;';
+        
+         
+     }
+             
+
+                document.getElementById('2').style = 'display:block;';
+                   document.getElementById('1').style = 'display:none;';
+*/
+}
